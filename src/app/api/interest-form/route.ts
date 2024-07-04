f68adcb67de7cd875b6316202b69c2bb1957c06a
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-04 15:50:25 -0400

    refactor: move gsheets client to lib

--- src/app/api/interest-form/route.ts
+++ src/app/api/interest-form/route.ts
@@ -1,7 +1,7 @@
-import * as sheets from './sheets';
-import sgMail from '@sendgrid/mail'
+import * as sheets from '@/lib/gsheets';
+import sgMail from '@sendgrid/mail';
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 export async function POST(req: Request) {
   const body = await req.json();
@@ -26,29 +26,27 @@ export async function POST(req: Request) {
 
     await sheets.appendRow(entry);
 
-    
-    
     const plainTextContent = `Hello ${name},
 
-Thank you for showing interest in joining our Web Development course! We're super excited to have you on board. We can't wait to start this journey with you from beginner to full-stack developer. 
+Thank you for showing interest in joining our Web Development course! We're super excited to have you on board. We can't wait to start this journey with you from beginner to full-stack developer.
 
-Here's some important information you need to know: 
-Start date: July 22nd 
-Duration: 6-8 weeks 
-Where: 100% online (make yourself as comfortable as possible) 
-How: Through our website and a Telegram group (more details coming soon) 
-Bonus: Prizes up for grabs! (Who doesn't love winning stuff, right?) 
+Here's some important information you need to know:
+Start date: July 22nd
+Duration: 6-8 weeks
+Where: 100% online (make yourself as comfortable as possible)
+How: Through our website and a Telegram group (more details coming soon)
+Bonus: Prizes up for grabs! (Who doesn't love winning stuff, right?)
 
-Whether you're just starting out or you've already dipped your toes in the coding pool, this course is designed to level up your skills. We'll take you from "What's HTML?" to "Look at this awesome full-stack website I built!" before you know it. 
+Whether you're just starting out or you've already dipped your toes in the coding pool, this course is designed to level up your skills. We'll take you from "What's HTML?" to "Look at this awesome full-stack website I built!" before you know it.
 
-Keep an eye on your inbox - we'll be sending more info about how to access the course materials as we get closer to the start date. 
+Keep an eye on your inbox - we'll be sending more info about how to access the course materials as we get closer to the start date.
 
-Remember, every expert was once a beginner. Your journey to becoming a skilled web developer starts here, and we're thrilled to guide you every step of the way. 
+Remember, every expert was once a beginner. Your journey to becoming a skilled web developer starts here, and we're thrilled to guide you every step of the way.
 
-Best regards, 
+Best regards,
 WazobiaCode`;
 
-      const htmlContent = `
+    const htmlContent = `
 <p>Hello ${name},</p>
 
 <p>Thank you for showing interest in joining our Web Development course! We're super excited to have you on board. We can't wait to start this journey with you from beginner to full-stack developer.</p>
@@ -70,20 +68,20 @@ WazobiaCode`;
 
 <p>Best regards,<br>WazobiaCode</p>`;
 
-      // Send confirmation email
-      const msg = {
-        to: email,
-        from: {
-          email: 'wazobiacode@gmail.com',
-          name: 'WazobiaCode'
-        },
-        subject: 'Welcome to Your Web Development Course!',
-        text: plainTextContent,
-        html: htmlContent,
-      }
-    await sgMail.send(msg)
+    // Send confirmation email
+    const msg = {
+      to: email,
+      from: {
+        email: 'wazobiacode@gmail.com',
+        name: 'WazobiaCode',
+      },
+      subject: 'Welcome to Your Web Development Course!',
+      text: plainTextContent,
+      html: htmlContent,
+    };
+    await sgMail.send(msg);
 
-    console.log("Email sent")
+    console.log('Email sent');
 
     return Response.json({
       status: 200,

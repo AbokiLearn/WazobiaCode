14885ec577c718cf02dd09cd3cf4b2725555deb7
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-05 21:10:51 -0400

    refactor: use env from @/lib/config to access env variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,10 @@ export const env = {
   APP_URL: process.env.APP_URL!,
   MONGODB_URI: process.env.MONGODB_URI!,
 
-  SHEET_ID: process.env.SHEET_ID!,
-  SHEET_NAME: process.env.SHEET_NAME!,
   GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
   GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL!,
+  SHEET_ID: process.env.SHEET_ID!,
+  SHEET_NAME: process.env.SHEET_NAME!,
+
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY!,
 };

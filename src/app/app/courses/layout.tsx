72187cd34ae928f27a5bab208aa254f322023a71
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-07 03:24:32 -0400

    feat: add page footers

--- src/app/app/courses/layout.tsx
+++ src/app/app/courses/layout.tsx
@@ -1,12 +1,7 @@
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-
-config.autoAddCss = false;
-
 export default function CourseCatalogLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="min-h-screen w-full">{children}</div>;
+  return <div>{children}</div>;
 }
---
+++ src/app/app/layout.tsx
@@ -0,0 +1,10 @@
+import { AppFooter } from '@/components/ui/app/footer';
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen w-full flex flex-col">
+      <main className="flex-grow">{children}</main>
+      <AppFooter />
+    </div>
+  );
+}

966cc679f35a249e90481d1d410acdd3f39dda43
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-08 17:03:50 -0400

    feat: add video player (iframe wrapper)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,8 @@ export function getYouTubeThumbnail(url: string): string {
   const videoId = url.split('v=')[1];
   return `https://img.youtube.com/vi/${videoId}/0.jpg`;
 }
+
+export function getYouTubeEmbedUrl(url: string): string {
+  const videoId = url.split('v=')[1];
+  return `https://www.youtube.com/embed/${videoId}`;
+}

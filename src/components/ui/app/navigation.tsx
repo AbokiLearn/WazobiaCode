51d08821cbd5ae72511b1918ac420de07abbbb88
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-08 14:58:49 -0400

    feat: update active section based on pathname

--- src/components/ui/app/navigation.tsx
+++ src/components/ui/app/navigation.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useSelectedLayoutSegment, usePathname } from 'next/navigation';
 import {
   Accordion,
   AccordionContent,
@@ -12,13 +16,26 @@ import { CourseWithSections } from '@/types/db/course';
 import { cn } from '@/lib/utils';
 
 const NavLinks = ({ course }: { course: CourseWithSections }) => {
+  const segment = useSelectedLayoutSegment();
+  const pathname = usePathname();
+  const [activeSection, setActiveSection] = useState<string>('');
+
+  useEffect(() => {
+    const currentSection = course.sections.find((section) =>
+      pathname.includes(`/${section.slug}`),
+    );
+    setActiveSection(currentSection?.slug || '');
+  }, [pathname, course.sections]);
+
   const NavItem = ({
     href,
     text,
+    isActive,
     className,
   }: {
     href: string;
     text: string;
+    isActive: boolean;
     className?: string;
   }) => {
     return (
@@ -26,6 +43,7 @@ const NavLinks = ({ course }: { course: CourseWithSections }) => {
         href={href}
         className={cn(
           'text-sm font-medium block py-1 mx-2 text-left hover:text-primary',
+          isActive ? 'text-primary font-bold' : '',
           className,
         )}
       >
@@ -35,30 +53,42 @@ const NavLinks = ({ course }: { course: CourseWithSections }) => {
   };
 
   return (
-    <>
-      {course.sections.map((section, index) => (
-        <AccordionItem key={index} value={section.slug}>
-          <AccordionTrigger className="text-md font-semibold text-left">
-            <NavItem
-              href={`/app/courses/${course.slug}/${section.slug}`}
-              text={section.title}
-              className="text-md font-semibold"
-            />
-          </AccordionTrigger>
-          <AccordionContent>
-            {section.lectures.map((lecture, index) => {
-              return (
-                <NavItem
-                  key={index}
-                  href={`/app/courses/${course.slug}/${section.slug}/${lecture.slug}`}
-                  text={lecture.title}
-                />
-              );
-            })}
-          </AccordionContent>
-        </AccordionItem>
-      ))}
-    </>
+    <Accordion
+      type="single"
+      collapsible
+      className="w-full"
+      value={activeSection}
+      onValueChange={setActiveSection}
+    >
+      {course.sections.map((section, index) => {
+        const isSectionActive = segment === section.slug;
+        return (
+          <AccordionItem key={index} value={section.slug}>
+            <AccordionTrigger className="text-md font-semibold text-left">
+              <NavItem
+                href={`/app/courses/${course.slug}/${section.slug}`}
+                text={section.title}
+                isActive={isSectionActive}
+                className="text-md font-semibold"
+              />
+            </AccordionTrigger>
+            <AccordionContent>
+              {section.lectures.map((lecture, index) => {
+                const isLectureActive = segment === lecture.slug;
+                return (
+                  <NavItem
+                    key={index}
+                    href={`/app/courses/${course.slug}/${section.slug}/${lecture.slug}`}
+                    text={lecture.title}
+                    isActive={isLectureActive}
+                  />
+                );
+              })}
+            </AccordionContent>
+          </AccordionItem>
+        );
+      })}
+    </Accordion>
   );
 };
 
@@ -67,9 +97,7 @@ export const SidebarNav = ({ course }: { course: CourseWithSections }) => {
     <div className="hidden border-r bg-muted md:flex md:flex-col md:w-[300px] md:min-h-screen overflow-y-auto">
       <div className="flex flex-col gap-2">
         <nav className="grid items-start text-sm font-medium px-2 lg:px-4">
-          <Accordion type="single" collapsible className="w-full">
-            <NavLinks course={course} />
-          </Accordion>
+          <NavLinks course={course} />
         </nav>
       </div>
     </div>
@@ -82,9 +110,7 @@ export const SheetNav = ({ course }: { course: CourseWithSections }) => {
       <HeaderLogo inSheet />
       <h1 className="text-2xl font-bold">{course.title}</h1>
       <nav className="grid gap-2 text-lg font-medium">
-        <Accordion type="single" collapsible className="w-full">
-          <NavLinks course={course} />
-        </Accordion>
+        <NavLinks course={course} />
       </nav>
       <div className="mt-auto mb-4">
         <SearchBar placeholder="Search course content..." inSheet />

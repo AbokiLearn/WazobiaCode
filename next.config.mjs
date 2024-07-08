72d4e37be56db52a1ed4a9b2d947d57e6544599e
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-08 14:23:26 -0400

    feat: add yt thumbnails to allowed image sources

--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,11 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+      },
     ],
   },
 };

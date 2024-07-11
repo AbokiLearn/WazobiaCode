7d7305a4eaa14dccd75fb6bc93a8ff6e67dbb657
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-11 17:11:11 -0400

    feat: add nav links

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -1,6 +1,9 @@
-import HeaderLogo from './header-logo';
-import { ProfileMenu, type User } from './profile-menu';
-import { Search } from './search';
+import Link from 'next/link';
+
+import { ProfileMenu, type User } from '@/components/app/profile-menu';
+import HeaderLogo from '@/components/app/header-logo';
+import { Search } from '@/components/app/search';
+import { cn } from '@/lib/utils';
 
 interface HeaderProps {
   title: string | React.ReactNode;
@@ -8,14 +11,44 @@ interface HeaderProps {
   children?: React.ReactNode;
 }
 
+export const HeaderLinks = ({ className }: { className?: string }) => {
+  const pages = [
+    {
+      label: 'Courses',
+      href: '/app/courses',
+    },
+    {
+      label: 'FAQ',
+      href: '/app/faq',
+    },
+  ];
+
+  return (
+    <>
+      {pages.map((page) => (
+        <Link
+          href={page.href}
+          key={page.href}
+          className={cn(
+            'text-primary-foreground hover:text-primary hover:bg-accent rounded-lg px-2 py-1',
+            className,
+          )}
+        >
+          {page.label}
+        </Link>
+      ))}
+    </>
+  );
+};
+
 export const Header = ({ title, user, children }: HeaderProps) => {
   return (
-    <header className="flex h-16 items-center gap-4 border-b bg-primary px-4 lg:h-[60px] lg:px-6">
+    <header className="flex h-16 items-center gap-4 border-b border-b-accent bg-primary px-4 lg:h-[60px] lg:px-6">
       {children}
-      <HeaderLogo />
-      <h2 className="font-semibold text-primary-foreground text-lg sm:text-xl md:text-2xl">
-        {title}
-      </h2>
+      <HeaderLogo className={cn('hidden', 'sm:block')} />
+      <div className="flex flex-row justify-between gap-6">
+        <HeaderLinks className="text-lg font-semibold" />
+      </div>
       <div className="ml-auto flex items-center gap-4">
         <Search />
         {user && <ProfileMenu user={user} />}

9c1171cf449ebd5da90b79ee0671da69c7af8874
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-11 21:20:05 -0400

    feat: add copy button

--- src/components/app/codeblock.tsx
+++ src/components/app/codeblock.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Copy, CopyCheck } from 'lucide-react';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/components/prism-javascript';
@@ -9,6 +10,7 @@ import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-cshtml';
+import { Button } from '@/components/ui/button';
 
 interface CodeBlockProps {
   children: string;
@@ -16,6 +18,8 @@ interface CodeBlockProps {
 }
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ children, className }) => {
+  const [copied, setCopied] = useState(false);
+
   useEffect(() => {
     Prism.highlightAll();
   }, [children]);
@@ -24,10 +28,25 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ children, className }) => {
     ? className.replace(/language-/, '')
     : 'javascript';
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(children);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1000);
+  };
+
   return (
-    <pre className={`language-${language} rounded-lg`}>
-      <code>{children}</code>
-    </pre>
+    <div className="relative">
+      <pre className={`language-${language} rounded-lg`}>
+        <code>{children}</code>
+      </pre>
+      <Button
+        onClick={copyToClipboard}
+        className="absolute top-2 right-2 hover:text-accent text-white rounded px-2 py-1 text-sm"
+        variant="link"
+      >
+        {copied ? <CopyCheck /> : <Copy />}
+      </Button>
+    </div>
   );
 };
 

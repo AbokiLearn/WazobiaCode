e0c8950b98d3e2d5c72de048e79d52a76e53cf12
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-14 16:36:50 -0400

    feat: implement role-based auth for /admin and /app

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,7 @@ export const env = {
   AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL!,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID!,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET!,
+  AUTH0_CUSTOM_CLAIMS_URI: process.env.AUTH0_CUSTOM_CLAIMS_URI!,
 
   APP_URL: process.env.APP_URL!,
   MONGODB_URI: process.env.MONGODB_URI!,
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,37 @@
-import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import {
+  withMiddlewareAuthRequired,
+  getSession,
+} from '@auth0/nextjs-auth0/edge';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withMiddlewareAuthRequired();
+import { env } from '@/lib/config';
+
+enum Role {
+  INSTRUCTOR = 'instructor',
+  STUDENT = 'student',
+}
+
+export default withMiddlewareAuthRequired(async function middleware(
+  req: NextRequest,
+) {
+  const res = NextResponse.next();
+  const session = await getSession(req, res);
+  const path = req.nextUrl.pathname;
+
+  const userRoles = session?.user[`${env.AUTH0_CUSTOM_CLAIMS_URI}/roles`] || [];
+
+  if (path.startsWith('/admin') && !userRoles.includes(Role.INSTRUCTOR)) {
+    return NextResponse.redirect(new URL('/', req.url));
+  } else if (path.startsWith('/app') && !userRoles.includes(Role.STUDENT)) {
+    if (userRoles.includes(Role.INSTRUCTOR)) {
+      return NextResponse.redirect(new URL('/admin', req.url));
+    }
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return res;
+});
 
 export const config = {
-  matcher: '/app/:path*',
+  matcher: ['/app/:path*', '/admin/:path*'],
 };

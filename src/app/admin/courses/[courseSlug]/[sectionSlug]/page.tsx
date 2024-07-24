9a5f8f5ed02e292e84ad51f638d649f5d5c1ceb9
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-24 16:18:07 -0400

    feat: implement lecture admin page

---
+++ src/app/admin/courses/[courseSlug]/[sectionSlug]/page.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { type JSONContent } from 'novel';
+import { toast } from 'sonner';
+
+import { LectureEditor } from '@/components/admin/courses/lecture-editor';
+import { SectionCard } from '@/components/admin/courses/section-card';
+import { Lectures } from '@/components/admin/courses/lectures';
+import { Breadcrumbs } from '@/components/admin/breadcrumbs';
+
+import { getSection, updateLecture } from '@/lib/client/course';
+import { ISection, ILecture } from '@/types/db/course';
+
+export default function CoursePage({
+  params,
+}: {
+  params: { courseSlug: string; sectionSlug: string };
+}) {
+  const [section, setSection] = useState<ISection | null>(null);
+  const [editingLecture, setEditingLecture] = useState<ILecture | null>(null);
+
+  const courseSlug = params.courseSlug;
+  const sectionSlug = params.sectionSlug;
+
+  useEffect(() => {
+    getSection(courseSlug, sectionSlug, true, true).then(setSection);
+  }, [courseSlug, sectionSlug]);
+
+  if (!section) {
+    return <div>Section not found</div>;
+  }
+
+  const saveLectureContent = async ({
+    content,
+    json_content,
+  }: {
+    content: string;
+    json_content: JSONContent;
+  }) => {
+    if (!editingLecture) return;
+
+    try {
+      console.log(content);
+      console.log(json_content);
+      const { lecture } = await updateLecture(
+        courseSlug,
+        sectionSlug,
+        editingLecture._id.toString(),
+        { content, json_content },
+      );
+      setEditingLecture(null);
+      toast.success('Lecture content saved');
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to save lecture content');
+    }
+  };
+
+  return (
+    <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8">
+      <div className="mb-8">
+        <Breadcrumbs />
+        <h1 className="text-3xl font-bold text-foreground">Edit Section</h1>
+      </div>
+      <div className="flex flex-col w-full">
+        <SectionCard section={section} />
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
+          <div className="lg:col-span-1">
+            <Lectures
+              courseSlug={courseSlug}
+              sectionSlug={sectionSlug}
+              setEditingLecture={setEditingLecture}
+              editingLecture={editingLecture}
+            />
+          </div>
+          <div className="lg:col-span-2">
+            <LectureEditor
+              lecture={editingLecture}
+              saveLectureContent={saveLectureContent}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

9faa2ebb68a74ef8549f280d4d6f6046352a7fff
Tony Okeke <muaddib@lisan-al-gaib>
2024-08-01 20:02:38 -0400

    feat: add timestamps to user_metadata

--- src/models/user-metadata.ts
+++ src/models/user-metadata.ts
@@ -2,20 +2,23 @@ import { Schema } from 'mongoose';
 
 import { IUserMetadata } from '@/types/db/user-metadata';
 
-export const UserMetadataSchema = new Schema<IUserMetadata>({
-  sub: { type: String, required: true, unique: true, index: true },
-  email: { type: String, required: true, unique: true },
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  phone_number: { type: String, required: true, unique: true },
-  telegram_user_id: {
-    type: String,
-    required: false,
-    default: null,
+export const UserMetadataSchema = new Schema<IUserMetadata>(
+  {
+    sub: { type: String, required: true, unique: true, index: true },
+    email: { type: String, required: true, unique: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    phone_number: { type: String, required: true, unique: true },
+    telegram_user_id: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    roles: [{ type: String, required: true }],
+    enrollments: [{ type: Schema.Types.ObjectId, ref: 'Enrollment' }],
   },
-  roles: [{ type: String, required: true }],
-  enrollments: [{ type: Schema.Types.ObjectId, ref: 'Enrollment' }],
-});
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
 UserMetadataSchema.index(
   { telegram_user_id: 1 },
   {

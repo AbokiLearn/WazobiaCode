5c39222cc764747cc5c3edefa8beca2cec15f8a4
Tony Okeke <muaddib@lisan-al-gaib>
2024-08-07 23:26:52 -0400

    feat: close registration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,6 @@ import LandingFAQ from '@/components/landing-page/faq';
 import { Footer } from '@/components/ui/footer';
 import { Button } from '@/components/ui/button';
 
-const LogInLink = () => {
-  return (
-    <Button
-      className="bg-accent text-white text-md"
-      onClick={() => {
-        window.location.href = '/api/auth/login';
-      }}
-    >
-      Register Now
-    </Button>
-  );
-};
-
 export default function LandingPage() {
   return (
     <>
@@ -49,8 +36,7 @@ export default function LandingPage() {
             <p className="mb-6">
               Learn to code at home - No experience required
             </p>
-            {/* <LearnMore /> */}
-            <LogInLink />
+            <LearnMore />
           </div>
         </section>
         <section className="mb-12">
@@ -97,7 +83,6 @@ export default function LandingPage() {
         <section className="w-full py-15 md:py-20 lg:py-20">
           <LandingFAQ id="faq-section" />
         </section>
-        {/*
         <section className="w-full">
           <Card className="max-w-2xl mx-auto">
             <CardHeader>
@@ -127,7 +112,6 @@ export default function LandingPage() {
             </CardContent>
           </Card>
         </section>
-        */}
       </div>
       <Footer />
     </>
